3bc46523f1e90558d55ed6cad585bb2ae090dfb0
Majsan3k <maja.lund@live.se>
2016-07-16 16:52:40 +0200

    Majsans första commit

--- src/window/Window.java
+++ src/window/Window.java
@@ -1,6 +1,10 @@
 package window;
 
+import Location.LocationView;
+
 import javax.swing.*;
+import java.awt.*;
+import java.awt.Color;
 
 /**
  * Created by simpa2k on 2016-07-16.
@@ -8,6 +12,11 @@ import javax.swing.*;
 public class Window extends JFrame {
 
     public Window() {
+
+        setLayout(new GridLayout(2,3));
+
+        
+
 
     }
 
