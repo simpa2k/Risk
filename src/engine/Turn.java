654cf6839fce4f8827f589717dd2db6a630a782d
Simon Olofsson <s.olofsson@gmail.com>
2016-10-31 20:07:48 +0100

    Added whitespace.

--- src/engine/Turn.java
+++ src/engine/Turn.java
@@ -5,12 +5,15 @@ import player.Player;
 /**
  * Created by simpa2k on 2016-07-16.
  */
+
 public class Turn {
 
     private Player player;
 
     public Turn(Player player) {
+
         this.player = player;
+
     }
 
     public void start() {
